5dbbb2a33d3778c80e10b10bb8a278a9e1b0eafa
Jungheon Kim <kjh990705@naver.com>
2022-05-06 23:27:16 +0900

    .

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -5,19 +5,9 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 const Activity = () => {
   return (
     <SafeAreaView>
-      <View style={{ flex: 1, flexDirection: 'column' }}>
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: 'powderblue',
-            flexDirection: 'row',
-          }}
-        >
-          <View style={{ flex: 1, backgroundColor: 'red' }} />
-          <View style={{ flex: 3, backgroundColor: 'blue' }} />
-        </View>
-        <View style={{ flex: 2, backgroundColor: 'skyblue' }} />
-        <View style={{ flex: 3, backgroundColor: 'steelblue' }} />
+      <View style={styles.container}>
+        <View style={styles.case1} />
+        <View style={styles.case2} />
       </View>
     </SafeAreaView>
   );
